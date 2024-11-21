fe5b10fb1303a88f1ae88d7dcf93af35232e1319
Diocleniax789 <julioramdom123@gmail.com>
2024-11-21 00:45:17 -0300

    se agrego un comentario sobre los distintos tipos de datos en JavaScript

--- variables.js
+++ variables.js
@@ -1,3 +1,17 @@
-let edad = 27;
+/*
 
+	Aqui se veran los tipos de datos y como declarar variables:
+
+	string - cadenas
+	number - numero
+	boolean - boolean(true or false)
+	objet - Objeto
+	function - fucniones
+
+	null - valor nulo
+	undefined - valor indefinido
+
+*/
+
+let edad = 27;
 console.log('Tu edad es: ',edad);
