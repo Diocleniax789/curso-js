3ac82a05aebbd82c0acd8628bc89af59c2bae4f5
Diocleniax789 <julioramdom123@gmail.com>
2024-11-21 23:38:29 -0300

    se han hecho algunos ejemplos de arreglo,s strings, numeros

---
+++ tiposDeDatos.js
@@ -0,0 +1,30 @@
+// cadena de texto
+
+const nombre = 'Julio';
+const parrafo = 'Este es un parrafo';
+
+//numero
+
+const num = 1;
+const dec = -75.40
+
+//boolean
+
+const usuarioConectado = true;
+
+const mayorDeEdad = 18;
+
+if (mayorDeEdad > 17) {
+
+	console.log('El es mayor de edad porque tiene: ',mayorDeEdad,'años');
+}
+
+// arreglos
+
+const personajes = ['Mikasa','Eren','Armin'];
+
+// en este arreglo se han puesto varios tipos de datos, un string, numero,un booleano, un objetom y un array de numeros
+const array_variado = ['Mikasa',4561,true,{character: 'Eren'},[1,2.,3]];
+
+console.log(array_variado);
+console.log('Los personajes de Attack On Titan son: ',personajes);
