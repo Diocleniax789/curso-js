09bfbfd6c1ccdb708f9f5cd33263e740802150ce
Diocleniax789 <julioramdom123@gmail.com>
2024-12-08 17:45:54 -0300

    ejercicio completado

--- crearListaDeAnimes.js
+++ crearListaDeAnimes.js
@@ -1,4 +1,4 @@
-let listaAnimes = [], tituloAnime, continuar = false,op;
+let listaAnimes = [], tituloAnime, continuar = false,op,nombreAnime,flag,pos, continuar2 = false,op2;
 
 while(continuar === false){
 	tituloAnime = prompt('Ingrese titulo del anime');
@@ -11,4 +11,31 @@ while(continuar === false){
 
 alert('Todos los animes han sido cargados');
 
-console.log(listaAnimes);
+
+do{
+
+	nombreAnime = prompt('Ingrese nombre del anime para ver si esta en la lista');
+
+	flag = false;
+	pos = 0;
+
+	do{
+		if (nombreAnime === listaAnimes[pos]) {
+			flag = true;
+			break;
+		} else{
+			pos++;
+		}
+
+	} while(pos < listaAnimes.length && flag === false);
+
+	flag === true ? alert('Anime encontrado!') : alert('Anime no encontrado');
+
+	op2 = confirm('Realizar otra busqueda?');
+
+	if(op2 === false){
+		continuar2 = true;
+	}
+
+} while(continuar2 === false);
+
