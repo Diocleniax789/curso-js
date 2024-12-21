b6845ca06ada556c31b465b23778fcda44b929a2
Diocleniax789 <julioramdom123@gmail.com>
2024-12-21 19:57:33 -0300

    se agregaron comentarios

--- apiReal.js
+++ apiReal.js
@@ -1,11 +1,14 @@
+// Esto simula una base de datos de videojuegos
 const baseDeDatosVideojuegos = [	
-	{ codigoProducto: '450190', nombre: 'Manhunt 2', desarrolladora: 'Rockstar Games',ESRB: '+18'},	
+	{ codigoProducto: '450190', nombre: 'Medal of Honor Allied Assault', desarrolladora: 'EA',ESRB: '+18'},	
 	{ codigoProducto: '785426', nombre: 'Call of Duty 2: Big Red One', desarrolladora: 'Activision',ESRB: '+18'},
 	{ codigoProducto: '542869', nombre: 'Enclave', desarrolladora: 'Starbreeze', ESRB: '+16'},
 	{ codigoProducto: '624569', nombre: 'Counter Strike 1.6', desarrolladora: 'Valve', ESRB: '+16'},
 	{ codigoProducto: '632484', nombre: 'Half-Life', desarrolladora: 'Valve', ESRB: '+16'},
 	{ codigoProducto: '858269', nombre: 'Battlefield V', desarrolladora: 'DICE', ESRB: '+18'}	
 ];
+
+// Esta funcion se encargara respectivamente de buscar el juego mediante el codigo de producto y devolvera una promesa
 
 function buscarVideojuego(codigoProducto) {
 	return new Promise((resolve,reject) => {
@@ -21,6 +24,7 @@ function buscarVideojuego(codigoProducto) {
 	});
 }
 
+// Esta funcion asincrona se encarga de ejecutar la busca y a esperar a que la promesa tenga un valor
 async function ejecutarBusqueda(codigoProducto) {
 	console.log('Realizando busqueda...');
 	try {
@@ -31,6 +35,7 @@ async function ejecutarBusqueda(codigoProducto) {
 	}
 }
 
+// Validacion para el codigo ingresado, que de ser correcto, se le pasara a la funcion ejecutarBusqueda para realizar las operaciones
 do {
 
 	let codProd = prompt('Ingrese codigo de producto');
